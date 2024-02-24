09a76401673e366b06047a57e39af62f89dee246
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-02-23 16:24:00 -0800

    Js code to select, display random image

--- js/script.js
+++ js/script.js
@@ -3,9 +3,34 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function() {
-    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+const getImage = async function () {
+    const res = await fetch(
+        "https://picsum.photos/v2/list?limit=100"
+    );
     const images = await res.json();
-    console.log(images);
+    // console.log(images);
+    selectRandomImage(images);
 }
-getImage();
+
+
+// function to randomly select the index of an image
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    // console.log(randomIndex); for test purpose only
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+    console.log(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove ("hide");
+    
+};
+
+button.addEventListener("click", function() {
+    getImage();
+});
